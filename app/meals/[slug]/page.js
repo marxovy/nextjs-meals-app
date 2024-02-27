03e116d71833e1fb7fb0bcd5b07f1bcf1c88fe40
Yoan Markov <yoannmarkoff@gmail.com>
2024-02-27 13:40:53 +0200

    feat: add Community page, add Landing Page, add Navigation

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+const MealsSlugPage = ({ params }) => {
+  return (
+    <main>
+      <h1>Meals subpage: {params.slug}</h1>
+    </main>
+  );
+};
+
+export default MealsSlugPage;
---
+++ app/meals/share/page.js
@@ -0,0 +1,5 @@
+const MealsSharePage = () => {
+  return <h1>Meals:Share Page</h1>;
+};
+
+export default MealsSharePage;
