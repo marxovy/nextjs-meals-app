c4ccad486a13cdaf29264ba429f4aaa9a7982a9e
Yoan Markov <yoannmarkoff@gmail.com>
2024-02-27 15:48:29 +0200

    feat: implement meal details/slug, add simple database store

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,35 @@
-const MealsSlugPage = ({ params }) => {
+import { getMeal } from "@/lib/meals";
+
+import Image from "next/image";
+import classes from "./page.module.css";
+
+const MealSlugPage = ({ params }) => {
+  const meal = getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Meals subpage: {params.slug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 };
 
-export default MealsSlugPage;
+export default MealSlugPage;
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,30 @@
-const MealsPage = () => {
-  return <h1>Meals Page</h1>;
+import { getMeals } from "@/lib/meals";
+import MealsGrid from "../components/meals/meals-grid";
+import classes from "./page.module.css";
+import { Suspense } from "react";
+
+const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+const MealsPage = async () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals, created </h1>
+        <span className={classes.highlight}>by you</span>
+        <p>Choose your favourite recipe and cook it yourself.</p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
